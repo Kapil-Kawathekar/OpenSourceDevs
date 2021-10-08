c31d08cd56eefc3413e99ff6835cf1a2ff4628ce
Kapil-Kawathekar <kapilkawathekar8@gmail.com>
2021-10-08 19:40:03 +0530

    bugFix for #30 and added react-router for #23

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,27 +1,27 @@
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import { Link } from '@reach/router';
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import { Link } from "react-router-dom";
 
 const navLinkStyle = {
-  color: 'white',
-  textDecoration: 'none',
+  color: "white",
+  textDecoration: "none",
 };
 
 export default function Navbar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position='static' color='primary'>
+      <AppBar position="static" color="primary">
         <Toolbar>
-          <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
-            <Link to='/' style={navLinkStyle}>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <Link to="/" style={navLinkStyle}>
               Open Source Devs
             </Link>
           </Typography>
-          <Button color='inherit'>
-            <Link to='about' style={navLinkStyle}>
+          <Button color="inherit">
+            <Link to="/about" style={navLinkStyle}>
               About
             </Link>
           </Button>
